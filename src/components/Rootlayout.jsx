42714a033665eb3eea1c69e2cabf6284ede26b54
samiris7 <samirsamirjoshi@gmail.com>
2024-08-15 20:51:48 +0545

    update

--- src/components/Rootlayout.jsx
+++ src/components/Rootlayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from './Header'
+
 import { Outlet } from 'react-router'
 
 const Rootlayout = () => {
   return (
     <div>
-      <Header />
+
       <Outlet />
     </div>
   )
